9a19d35ab7d4803b3317da1c72ffe5a0f9c5b274
Sébastien Wagener <sebastien.wagener@gmail.com>
2025-07-20 17:23:56 +0200

    Use describe-it in test

--- src/src/App.test.tsx
+++ src/src/App.test.tsx
@@ -6,13 +6,16 @@ import store from './store';
 import { render } from './test-utils/render';
 import { screen } from '@testing-library/react';
 
-test('renders table', () => {
-  render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  );
-  expect(screen.getByRole('table')).toBeInTheDocument();
+describe('App component', () => {
+  it('renders table', () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    );
+    expect(screen.getByRole('table')).toBeInTheDocument();
+  });
+
 });
