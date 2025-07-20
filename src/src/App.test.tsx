21287db3b43287009fb3b0ce39dd9b42aeb596f7
Sébastien Wagener <sebastien.wagener@gmail.com>
2025-07-20 16:09:08 +0200

    Add jest + react testing tests

--- src/src/App.test.tsx
+++ src/src/App.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders table', () => {
+  render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+  expect(screen.getByRole('table')).toBeInTheDocument();
 });
